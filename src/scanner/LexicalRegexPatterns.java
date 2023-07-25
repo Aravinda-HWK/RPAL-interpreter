564002eef49a8b4d0365ebee9a1b411a60afa7f1
Aravinda-HWK <118424430+Aravinda-HWK@users.noreply.github.com>
2023-07-25 10:10:41 +0530

    Lixical Analyser is created

--- src/scanner/LexicalRegexPatterns.java
+++ src/scanner/LexicalRegexPatterns.java
@@ -2,38 +2,27 @@ package scanner;
 
 import java.util.regex.Pattern;
 
-/**
- * Regex matchers that comply with RPAL's lexical grammar. Used by the scanner to
- * tokenize the input.
- * 
- * @author Group 9
- */
-public class LexicalRegexPatterns{
-  private static final String letterRegexString = "a-zA-Z";
-  private static final String digitRegexString = "\\d";
-  private static final String spaceRegexString = "[\\s\\t\\n]";
-  private static final String punctuationRegexString = "();,";
-  private static final String opSymbolRegexString = "+-/~:=|!#%_{}\"*<>.&$^\\[\\]?@";
-  private static final String opSymbolToEscapeString = "([*<>.&$^?])";
-  
-  public static final Pattern LetterPattern = Pattern.compile("["+letterRegexString+"]");
-  
-  public static final Pattern IdentifierPattern = Pattern.compile("["+letterRegexString+digitRegexString+"_]");
+public class LexicalRegexPatterns {
+    // Regular expression character classes for different types of tokens
+    private static final String LETTER = "a-zA-Z";                                 // Letters (uppercase and lowercase)
+    private static final String DIGIT = "\\d";                                     // Digits
+    private static final String OPERATOR_SYMBOL = "+-*<>&.@/:=~|$!#%^_\\[\\]{}\"'?"; // Operator symbols
+    private static final String PUNCTUATION = "();,";                              // Punctuation symbols
+    private static final String SPACES = "\\s";                                    // Whitespace characters (spaces, tabs, newlines)
 
-  public static final Pattern DigitPattern = Pattern.compile(digitRegexString);
-
-  public static final Pattern PunctuationPattern = Pattern.compile("["+punctuationRegexString+"]");
-
-  public static final String opSymbolRegex = "[" + escapeMetaChars(opSymbolRegexString, opSymbolToEscapeString) + "]";
-  public static final Pattern OpSymbolPattern = Pattern.compile(opSymbolRegex);
-  
-  public static final Pattern StringPattern = Pattern.compile("[ \\t\\n\\\\"+punctuationRegexString+letterRegexString+digitRegexString+escapeMetaChars(opSymbolRegexString, opSymbolToEscapeString) +"]");
-  
-  public static final Pattern SpacePattern = Pattern.compile(spaceRegexString);
-  
-  public static final Pattern CommentPattern = Pattern.compile("[ \\t\\'\\\\ \\r"+punctuationRegexString+letterRegexString+digitRegexString+escapeMetaChars(opSymbolRegexString, opSymbolToEscapeString)+"]"); //the \\r is for Windows LF; not really required since we're targeting *nix systems
-  
-  private static String escapeMetaChars(String inputString, String charsToEscape){
-    return inputString.replaceAll(charsToEscape,"\\\\\\\\$1");
-  }
+    // Patterns for different token types
+    public static final String opSymbolRegex = "[" + escapeMetaChars(OPERATOR_SYMBOL) + "]";                         // Escapes special characters in operator symbols
+    public static final Pattern OpSymbolPattern = Pattern.compile(opSymbolRegex);                                    // Matches operator symbols
+    public static final Pattern LetterPattern = Pattern.compile("[" + LETTER + "]");                                 // Matches single letters
+    public static final Pattern IdentifierPattern = Pattern.compile("[" + LETTER + DIGIT + "_]");                    // Matches identifiers (letters, digits, underscores)
+    public static final Pattern StringPattern = Pattern.compile("[ \t\n\\" + PUNCTUATION + LETTER + DIGIT + escapeMetaChars(OPERATOR_SYMBOL) + "]");  // Matches strings (including escape characters)
+    public static final Pattern SpacePattern = Pattern.compile(SPACES);                                                // Matches whitespace characters
+    public static final Pattern DigitPattern = Pattern.compile(DIGIT);                                                // Matches single digits
+    public static final Pattern PunctuationPattern = Pattern.compile("[" + PUNCTUATION + "]");                       // Matches punctuation symbols
+    public static final Pattern CommentPattern = Pattern.compile("[ \t'\\\\ \r" + PUNCTUATION + LETTER + DIGIT + escapeMetaChars(OPERATOR_SYMBOL) + "]");  // Matches comments (including escape characters)
+    
+    // Helper method to escape special characters in the OPERATOR_SYMBOL
+    private static String escapeMetaChars(String inputString) {
+        return inputString.replaceAll("([*<>.&$^?])", "\\\\\\\\$1");
+    }
 }
